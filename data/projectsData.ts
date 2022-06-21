ecc802f23c4cd8ba4e6f1feaba952531cc1224f5
reubence <reuben3699@gmail.com>
2022-06-21 12:37:31 +0530

    Projects Data Updated

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,21 +2,21 @@ const projectsData = [
   {
     title: 'Branding',
     description:
-      'Branding is a process of creating a brand identity for a business. It is the way that a business looks and feels. It is the way that a business communicates with the world. This project is a complex marketing website built with Next.js, Framer-Motion. It is a fully responsive website.',
+      'Branding is a process of creating a brand identity for a business. This project is a fully responsive & complex marketing website with smooth animations built with Next.js & Framer-Motion.',
     imgSrc: '/static/images/projects/Branding.png',
     href: 'https://github.com/reubence/awwards-website-recreation',
   },
   {
     title: 'Fin-Tech',
     description:
-      'An enterprise resource planning software is used by businesses to manage their daily processes including Accounting, Invoice Generation & Tax Filing. This software was built to be fully responsive using Next.js & Supabase and aims to automate various processes and enhance productivity of employees.',
+      'An enterprise resource planning software is used by businesses to manage their daily processes including Accounting, Invoice Generation & Tax Filing. This software was built to be fully responsive using Next.js & Supabase and aims to automate various processes and enhance productivity of employeess.',
     imgSrc: '/static/images/projects/FinTech.png',
     href: 'https://github.com/reubence/ERP',
   },
   {
     title: 'Blockchain',
     description:
-      'A single page website that mints NFTs for the user on the lasrgest marketplace OpenSea and is fully responsive. Built using Next.js, Framer-Motion and Supabase.',
+      'A single page website that mints NFTs for the user on the largest marketplace (OpenSea) and is fully responsive. Built using Next.js, Framer-Motion and Supabase.',
     imgSrc: '/static/images/projects/Blockchain.png',
     href: 'https://github.com/reubence/nft-minting-live',
   },
