f2e249b408c32b9390d99bbf9d9440963a06b50b
Reuben Rapose <54074556+reubence@users.noreply.github.com>
2022-06-29 13:26:23 +0530

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   author: 'Reuben Rapose',
   headerTitle: 'Reuben Rapose',
   description:
-    'Reuben is a Data Scientist by training and Full-Stack Developer by profession. In his early teens, while pursuing his obsession with technology, he discovered a passion for building tech that makes a difference – Today, Reuben works as an independent Consultant, get in touch. ✌︎',
+    'Reuben is a Data Scientist by training and Full-Stack Developer by passion. In his early teens, while pursuing his obsession with technology, he discovered a passion for building tech that makes a difference – Today, Reuben works as an independent Consultant, get in touch. ✌︎',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://reubence.com',
